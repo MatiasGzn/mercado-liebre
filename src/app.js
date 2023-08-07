0cdebfc19106aaa3eb2b1a0d8ab6fcfffc1524cb
MatiasGzn <garzonmatias1811@gmail.com>
2023-08-06 21:43:51 -0300

    agregando archivos del proyecto

---
+++ src/app.js
@@ -0,0 +1,27 @@
+//Requiero los paquetes:
+const express = require("express");
+const path = require("path")
+
+const app = express();
+
+//Declarar los recursos estaticos:
+app.use(express.static("public"))
+
+//Poner a escuchar al servidor:
+app.listen(3032, () => console.log("Servidor corriendo"))
+
+//Manejo de rutas:
+//Ruta raiz "/"":
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, "views/home.html"))
+})
+
+//Ruta register:
+app.get("/register", (req, res) => {
+    res.sendFile(path.join(__dirname, "views/register.html"))
+})
+
+//Ruta ligin:
+app.get("/login", (req, res) => {
+    res.sendFile(path.join(__dirname, "views/login.html"))
+})
